5b981631c214b9444d4f05caca37d44c36a26e16
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-10 14:13:06 +0530

    implemantedwork

--- src/Component/HeaderSection/MainBodyHeader/MainHeader.jsx
+++ src/Component/HeaderSection/MainBodyHeader/MainHeader.jsx
@@ -32,6 +32,29 @@ export const MainHeader = () => {
                   <Button variant="info">Login</Button>{' '}
                   <br/><br/>
                   <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                   <button className="forget">Forget password</button>  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                   <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
+                  <button className="forget">Forget password</button> 
                 </Form>
             </div>
         </div>
