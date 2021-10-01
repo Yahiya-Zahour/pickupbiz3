d4838b818fe6d6debcbec67c6e7273fc676de6b8
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-01 12:10:22 +0530

    practice

--- src/Component/Homepage/Landing.jsx
+++ src/Component/Homepage/Landing.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import {MainHeader} from '../HeaderSection/MainBodyHeader/MainHeader';
-import {BodySection} from '../BodySection/BodySection';
+import {MainHeader} from '../HeaderSection/MainBodyHeader/MainHeader';
+// import {BodySection} from '../BodySection/BodySection';
 
 
 
  export const Landing = () => {
     return <div>
-            {/* <MainHeader/> */}
-            <BodySection />
+            <MainHeader/>
+            {/* <BodySection /> */}
         </div>;
 };
